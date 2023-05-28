b6691318b28812ee22c4b9e81186bd8e296af3a3
Angry Coding <mailidanielifechukwu@gmail.com>
2023-05-29 00:16:08 +0100

    chamges

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EverMoon Fork
+
+to
--- contracts/EverMoon.sol
+++ contracts/EverMoon.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity 0.8.18;
 
 // https://twitter.com/EverMoonERC
 // https://t.me/EverMoonERC
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IUniswapV2Router02 {
